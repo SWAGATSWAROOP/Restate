6c3a24580d4c5851550f0c1b9bfefe06581fdee8
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2025-01-06 14:35:48 +0530

    Started ReactNative

---
+++ lib/appwrite.ts
@@ -0,0 +1,16 @@
+import { Client } from "react-native-appwrite";
+
+export const config = {
+  platform: "com.swagat.restate",
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
+
+export const client = new Client();
+
+client
+  .setEndpoint(config.endpoint!)
+  .setProject(config.projectId!)
+  .setPlatform(config.platform!);
+
+
